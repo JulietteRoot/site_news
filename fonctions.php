8c5dc5df2476a96c3b8d0f21d5d603df2416362e
Juliette <julie@nuguet.fr>
2012-07-25 19:29:09 +0200

    qqs modifs

--- fonctions.php
+++ fonctions.php
@@ -2,7 +2,15 @@
 
 function connection()
 {
-	return new PDO('mysql:host=localhost;dbname=site_news','root','meat_boy');
+	try
+	{
+		$bdd = new PDO('mysql:host=localhost;dbname=site_news','root','meat_boy');
+	}
+	catch (Exception $e)
+	{
+		die('Erreur:'.$e->getMessage());
+	}
+	return $bdd;
 }
 
 ?>
