f4974266eb10b469bc8aeb5c4287e205242acb8a
JulietteRoot <juliette.root@gmail.com>
2012-08-01 18:10:57 +0200

    ajout class Mabdd

---
+++ Mabdd.class.php
@@ -0,0 +1,17 @@
+<?php
+
+class Mabdd
+{
+	public function connection()
+	{
+		try
+		{
+			$bdd = new PDO('mysql:host=localhost;dbname=site_news','root','meat_boy');
+		}
+		catch (Exception $e)
+		{
+			die('Erreur:'.$e->getMessage());
+		}
+		return $bdd;
+	}
+}
--- fonctions.php
+++ fonctions.php
@@ -1,18 +1,5 @@
 <?php
 
-function connection()
-{
-	try
-	{
-		$bdd = new PDO('mysql:host=localhost;dbname=site_news','root','meat_boy');
-	}
-	catch (Exception $e)
-	{
-		die('Erreur:'.$e->getMessage());
-	}
-	return $bdd;
-}
-
 function definition_pseudo($pseudo_indique)
 // La variable $pseudo est celle indiquée, ou à défaut "Anonyme".
 {
